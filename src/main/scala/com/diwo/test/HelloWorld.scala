137e7f9a62eaab3dca6c567cfb1d925668f19870
Harish Siravuri <hsiravuri@getdiwo.com>
2019-02-07 18:42:55 -0500

    Test

---
+++ src/main/scala/com/diwo/test/HelloWorld.scala
@@ -0,0 +1,17 @@
+/*
+ * HelloWorld.scala
+ *
+ * Copyright (c) 2018. Loven Systems - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Harish, 2018
+ */
+
+package com.diwo.test
+
+object HelloWorld extends App {
+
+  println("Hello World!")
+
+}
